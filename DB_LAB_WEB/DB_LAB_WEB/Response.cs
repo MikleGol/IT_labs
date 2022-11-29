d8fcfcc8cd22ea3155008a0ca3dac126db2fb13b
MikleGol <63545026+MikleGol@users.noreply.github.com>
2022-11-29 21:13:40 +0200

    Delete DB_LAB_WEB directory

--- DB_LAB_WEB/DB_LAB_WEB/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace DBMS_Web_API {
-    public class Response<T> {
-        public T Value { get; set; }
-
-        public IDictionary<string, string> Links { get; set; }
-    }
-}
